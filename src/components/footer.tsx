fc99eb1f25f62b008dbb12c31253bb607eef30cb
Felipe Scherer <felipescherer25@gmail.com>
2024-04-06 23:03:59 -0300

    :sparkles: feat: metadata and location

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import { LucideMapPin } from 'lucide-react'
 import Link from 'next/link'
 
 export function Footer() {
@@ -6,6 +7,12 @@ export function Footer() {
   return (
     <div className="bg-text text-textOnPrimary">
       <footer className="m-auto flex flex-col items-center gap-2 p-6">
+
+        <Link href="https://maps.app.goo.gl/7h7u53r5nR3koNsN9" target="_blank" className="link flex items-center gap-1">
+          <LucideMapPin size={14} />
+          <small>Av Prefeito Erasmo Chrispim nº100, Itatiba, SP, Brasil</small>
+        </Link>
+
         <div>
           <small>
             Copyright © 🞄 AMIGOS DO SUL GRILL 🞄
